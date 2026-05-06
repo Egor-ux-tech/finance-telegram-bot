7da929e47cf1452f4bd28109362d1c217919d162
Егор <egor@MacBook-Air-Egor.local>
2026-05-06 10:22:36 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,53 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from main import load_data, add_expense
+import os
+TOKEN = os.getenv("8501080390:AAE-E2FHMhTaPOImhP4T6zX7LKtYXV2wgss")
+file_name = "expenses.json"
+
+keyboard = [
+    ["Добавить расход"],
+    ["Показать расходы"],
+    ["Общая сумма расходов"]
+]
+markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Привет, я трекер расходов\n\n Выбери действие:",
+        reply_markup=markup
+    )
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    if text == "Добавить расход":
+        await update.message.reply_text("Введи категорию и цену\n")
+
+    elif text == "Показать расходы":
+        data = load_data()
+        if not data:
+            await update.message.reply_text("Нет расходов")
+            return 
+        result = "Твои расходы:\n"
+        for item in data:
+            result += f"{item['category']} - {item['price']}\n"
+        await update.message.reply_text(result)
+
+    elif text == "Общая сумма расходов":
+        data = load_data()
+        total = sum(item["price"] for item in data)
+        await update.message.reply_text(f"Общая сумма: {total}")
+    
+    else:
+        try:
+            parts = text.split()
+            category = parts[0]
+            price = float(parts[1])
+        except:
+            await update.message.reply_text("Ошибка. Введи категорию и цену")
+            return
+        data = load_data()
+        add_expense(data, category, price)
+        await update.message.reply_text("Расход добавлен")
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, handle_message))
+app.run_polling()
---
+++ main.py
@@ -0,0 +1,76 @@
+import json
+import os
+file_name="expense.json"
+def load_data():
+    if not os.path.exists(file_name):
+        return[]
+    with open(file_name, "r") as file:
+        return json.load(file)
+def save_data(data):
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=4)
+def get_categories(data):
+    categories = set()
+    for item in data:
+        categories.add(item["category"])
+    return list(categories)
+def add_expense(data, category, price):
+    categories = get_categories(data)
+   # if  categories:
+        #print("\n Выбери категорию или введи новую:")
+        #for i, cat in enumerate(categories, 1):
+           # print(f"{i}. {cat}")
+       ## print("0. Новая категория")
+        #choice = input("Выбор: ")
+        #if choice == "0":
+        #    category = input("Новая категория: ")
+        #else:
+         #   try:
+         #       category = categories[int(choice) - 1]
+          #  except:
+           #     print("Ошибка выбора")
+           #     return
+   # else:
+      #  category = input("Категория: ")
+   # try:
+     #   price = float(input("Цена: "))
+   # except:
+     ##  return
+    expense = {
+        "category": category,
+        "price": price
+    }
+    data.append(expense)
+    save_data(data)
+    print("Расход добавлен")
+def show_expenses(data):
+    if not data:
+        print("Расходов нет")
+        return
+    for item in data:
+        print(f"{item['category']} - {item['price']}")
+def show_sum(data):
+    sum=0
+    for item in data:
+        sum+=item['price']
+    print("Общая сумма:", sum)
+#def main():
+    #data=load_data()
+    #while True:
+      #  print("\n1. Добавить расход")
+      #  print("2. Показать расходы")
+       # print("3. Общая сумма расходов")
+       # print("4. Выход")
+        #choice = input("Выбор:")
+        #if choice == "1":
+        #    add_expense(data)
+       # elif choice == "2":
+       #     show_expenses(data)
+        #elif choice == "3":
+        #    show_sum(data)
+       # elif choice == "4":
+        #    break
+        #else:
+           # print("Ошибка")
+#if __name__ == "__main__":
+   # main()
